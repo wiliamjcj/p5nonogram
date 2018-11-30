f9d0c7efce445744293c28b451fd6b049300ce92
wiljcj <wiljc@gmail.com>
2018-11-30 01:01:09 -0200

    adding board

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 let canvasX = 800
 let canvasY = 600
-let boardSize = 5
+let boardSize = 10
 let board = []
 let squareSize = 30
-let margin = 30
-let squareGapLimit = 100
-let squareGapSize = 10
+let margin = 10
+let gapLimit = 5
+let gapSize = 10
 
 function setup() {
     createCanvas(canvasX, canvasY)
     board = createBoard()
+    console.log("asd", board.length)
 }
 
 function draw() {
-    background(230)
-    fill(20)
+    background(200)
+    
+    let gapX = 0
     for (let i = 0; i < board.length; i++) {
         let inner = board[i]
+        let gapY = 0
+
+        if(i > 0 && i%gapLimit === 0){
+            gapX += gapSize 
+        }
+
         for (let j = 0; j < inner.length; j++) {
-            let x,y,w,h = 0
-            
-            if(i%squareGapLimit === 0){
-                x = i*squareSize + margin + squareGapSize
-                w = squareSize + squareGapSize
-            }else{
-                x = i*squareSize + margin
-                w = squareSize
+
+            if(j > 0 && j%gapLimit === 0){
+                gapY += gapSize 
             }
-            
-            if(j%squareGapLimit === 0){
-                y = j*squareSize + margin + squareGapSize
-                h = squareSize + squareGapSize
-            }else{
-                y = j*squareSize + margin
-                h = squareSize
-            }
-            console.log(i%squareGapLimit)            
-            rect(x, y, w, h)
+
+            x =  i*squareSize + margin + gapX
+            y =  j*squareSize + margin + gapY
+            push()
+            fill(240)
+            stroke(210)
+            rect(x, y, squareSize, squareSize)
+            pop()
         }   
     }
 }
